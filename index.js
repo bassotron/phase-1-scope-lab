d5ffcdefef5cd4f61e9e0ababcba3c3f49af9a19
Will Lovell <willard.lovell@icloud.com>
2021-12-01 18:30:15 -0700

    commit tree 2560cc9677d4e3f07cb18d5cb7145c069aaee112

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+
+
+
+var customerName = "bob"
+const leastFavoriteCustomer = "will"
+
+
+function upperCaseCustomerName() {
+      customerName = "BOB";
+
+}
+
+function setBestCustomer() {
+       bestCustomer = "not bob"
+     return bestCustomer
+
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+    return bestCustomer
+}
+
+function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = "you";
+    return leastFavoriteCustomer
+}
